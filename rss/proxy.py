2d5e29fff3b21e24492a56308e7dba79960ca644
herber523 <herber523@gmail.com>
2018-01-10 10:43:19 +0800

    add args

--- rss/proxy.py
+++ rss/proxy.py
@@ -5,7 +5,7 @@ from redis.sentinel import Sentinel
 class SentinelProxy:
     """Proxy for Redis sentinel."""
 
-    def __init__(self, sentinel_host, master_name, socket_timeout=0.1):
+    def __init__(self, sentinel_host, master_name, socket_timeout=0.1, **args):
         """Initialize Redis sentinel connection.
 
         :params: sentinel_host: (host, port)
@@ -13,9 +13,9 @@ class SentinelProxy:
         self.sentinel = Sentinel(
             [sentinel_host], socket_timeout=socket_timeout)
         self.master = self.sentinel.master_for(master_name,
-                                               socket_timeout=socket_timeout)
+                                               socket_timeout=socket_timeout, **args)
         self.slave = self.sentinel.slave_for(master_name,
-                                             socket_timeout=socket_timeout)
+                                             socket_timeout=socket_timeout, **args)
 
     def __getattr__(self, name):
         """Get attribute from Redis master or slave."""
